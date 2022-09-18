5d1d0b7455e6f43687e8037aae5330aa828b8849
joseywales666 <33660300+joseywales666@users.noreply.github.com>
2022-09-19 00:11:42 +0100

    added sensor rest requests. created sensor db and query db. Query requests implemented.

--- src/__init__.py
+++ src/__init__.py
@@ -14,14 +14,25 @@ app = Flask(__name__)
 # Create API!
 api = Api(app)
 
-def get_db():
+def get_sensor_db():
 
     # g is a flask object for storing data during the application context of a running Flask web app
     
     db = getattr(g, '_database', None)
 
     if db is None:
-        db = g._database = shelve.open("mock.db")
+        db = g._database = shelve.open("sensor.db")
+
+    return db
+
+def get_query_db():
+
+    # g is a flask object for storing data during the application context of a running Flask web app
+    
+    db = getattr(g, '_database', None)
+
+    if db is None:
+        db = g._database = shelve.open("query.db")
 
     return db
 
@@ -74,6 +85,8 @@ def cli_interface():
 
     args = arg_parser.parse_args()
 
+    return args
+
 def make_response(query):
     metric, stat, start, end = query['Metric'],query['Statistic'],query['Start Date'],query['End Date']
     #TODO Implement this!
@@ -85,7 +98,7 @@ class Query(Resource):
 
     def get(self, query_id):
 
-        shelf = get_db()
+        shelf = get_query_db()
         
         # If the key does not exist in the data store, return a 404 error.
         if (query_id not in shelf):
@@ -98,7 +111,7 @@ class Query(Resource):
 class QueryList(Resource):
 
     def get(self):
-        shelf = get_db()
+        shelf = get_query_db()
         keys = list(shelf.keys())
 
         queries = []
@@ -113,13 +126,18 @@ class QueryList(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument('SensorID', required=True)
         parser.add_argument('Metric', required=True)
+        parser.add_argument('Statistic', required=True)
         parser.add_argument('Start Date', required=False)
         parser.add_argument('End Date', required=False)
 
         # get db
-        shelf = get_db()
+        shelf = get_query_db()
         # create query from arg parser
         query = parser.parse_args()
+        #make unique(ish) query_id
+        query_id = query['SensorID'] + datetime.now().strftime("%m%d%Y%H%M%S")
+        
+        query['QueryID'] = (query_id)
         # store query in db on QueryID
         shelf[query["QueryID"]] = query
 
@@ -131,7 +149,7 @@ class QueryList(Resource):
         return {'message': 'Query successful', 'data': response}, 201
 
     def delete(self, query_id):
-        shelf = get_db()
+        shelf = get_query_db()
 
         # If the key does not exist in the data store, return a 404 error.
         if (query_id not in shelf):
@@ -143,11 +161,63 @@ class QueryList(Resource):
 
 #########################################################################################
 
+class SensorList(Resource):
+
+    def get(self):
+        shelf = get_sensor_db()
+        keys = list(shelf.keys())
+
+        sensors = []
+
+        for key in keys:
+            sensors.append(shelf[key])
+
+        return {'message': 'Success', 'data': sensors}, 200
+
+    def post(self):
+        parser = reqparse.RequestParser()
+
+        parser.add_argument('SensorID', required=True)
+        parser.add_argument('Gateway', required=True)
+        parser.add_argument('Queries', required=False)
+
+        # Parse the arguments into an object
+        args = parser.parse_args()
+
+        shelf = get_sensor_db()
+        shelf[args["SensorID"]] = args
+
+        return {'message': 'Sensor registered', 'data': args}, 201
+
+#########################################################################################
+
+class Sensor(Resource):
+    def get(self, sensor_id):
+        shelf = get_sensor_db()
+
+        # If the key does not exist in the data store, return a 404 error.
+        if (sensor_id not in shelf):
+            return {'message': 'Sensor not found', 'data': {}}, 404
+
+        return {'message': 'Sensor found', 'data': shelf[sensor_id]}, 200
+
+    def delete(self, sensor_id):
+        shelf = get_query_db()
+
+        # If the key does not exist in the data store, return a 404 error.
+        if (sensor_id not in shelf):
+            return {'message': 'Sensor not found', 'data': {}}, 404
+
+        del shelf[sensor_id]
+        return '', 204
+
+#########################################################################################
+
 class DBCheck(Resource):
 
     def get(self):
 
-        shelf = get_db()
+        shelf = get_query_db()
         query_keys = list(shelf.keys())
 
         queries = []
@@ -159,5 +229,7 @@ class DBCheck(Resource):
 
 #########################################################################################
 
-api.add_resource(QueryList, '/queries/<sensor_id>')
-api.add_resource(Query, '/query/<query_id>')
+api.add_resource(QueryList, '/sensor/queries')
+api.add_resource(Query, '/sensor/query/<query_id>')
+api.add_resource(SensorList, '/sensors' )
+api.add_resource(Sensor, '/sensor/<sensor_id>')
